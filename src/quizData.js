eafab7990bdecb8785faeb58b01a576c62293657
rasuloff789 <rasuloff789@gmail.com>
2020-12-19 11:39:21 +0500

    data added

---
+++ src/quizData.js
@@ -0,0 +1,82 @@
+export let quizData = [
+  {
+    question : "2 ga '2' ni qo'shsa nima bo'ladi",
+    id : 1 ,
+    answers : [
+      {answer : "Men matematikani o'qimaganman" , correct : false},
+      {answer : "albatta 4" , correct : false},
+      {answer : "farqni topolmadim" , correct : false},
+      {answer : "JS da 22 bo'ladi )))" , correct : true}
+    ]
+  },
+  {
+    question : "Dasturlash osonmi ?",
+    id : 2 ,
+    answers : [
+      {answer : "HA oson" , correct : false},
+      {answer : "Qiyin ham oson ham emas " , correct : false},
+      {answer : "Neytralman" , correct : false},
+      {answer : "Men dasturchi emasman" , correct : true}
+    ]
+  },
+  {
+    question : " Is HTML programming language ?",
+    id : 3 ,
+    answers : [
+      {answer : "Yes :/" , correct : false},
+      {answer : "NO :" , correct : true},
+      {answer : "Inglizchani tushunmayman ;)" , correct : false},
+      {answer : "Bilmayman :|" , correct : false}
+    ]
+  },
+  {
+    question : "Tashkent cityga kirish pullik bo'ldi , fikringiz qanday ?",
+    id : 4 ,
+    answers : [
+      {answer : "Yaxshi bo'ldi odamlar , mehnatning qadriga yetishadi ." , correct : false},
+      {answer : "5600 so'm pul topgan odam kambag'al emas ((" , correct : false},
+      {answer : "Nima ham qilib bilamiz &*#$" , correct : false},
+      {answer : "Zato , osmonimiz musappo ))" , correct : true}
+    ]
+  },
+  {
+    question : "Eng katta IT kompaniya qaysi",
+    id : 5 ,
+    answers : [
+      {answer : "Apple" , correct : true},
+      {answer : "Amazon" , correct : false},
+      {answer : "Google" , correct : false},
+      {answer : "Microsoft" , correct : false}
+    ]
+  },
+  {
+    question : "Najot Ta'limga nechanchi yil asos solingan ?",
+    id : 6 ,
+    answers : [
+      {answer : "2017" , correct : false},
+      {answer : "2019" , correct : false},
+      {answer : "2020" , correct : false},
+      {answer : "2018" , correct : true}
+    ]
+  },
+  {
+    question : "O'zbekistondagi eng katta monopoliya ?",
+    id : 7 ,
+    answers : [
+      {answer : "GM Uzbekistan" , correct : false},
+      {answer : "Artel" , correct : true},
+      {answer : "Bilmayman" , correct : false},
+      {answer : "Ikkovi ham" , correct : false}
+    ]
+  },
+  {
+    question : "Eng baland sharshara",
+    id : 8 ,
+    answers : [
+      {answer : "Anxel" , correct : true},
+      {answer : "Tuxel" , correct : false},
+      {answer : "Niagara" , correct : false},
+      {answer : "Viktoria" , correct : false}
+    ]
+  }
+]
